ae60d586dfa02837dce5cfc54df9ba5b52605ae1
db95311 <daniil.brusentsov@citi.com>
2021-02-04 13:39:45 +0300

    + testcase - invalid argument

--- src/test/java/console/framework/CommandHandlerTest.java
+++ src/test/java/console/framework/CommandHandlerTest.java
@@ -107,4 +107,78 @@ public class CommandHandlerTest {
 
         scenario.checkFinish();
     }
+
+    @Test
+    public void enterInvalidValue() {
+        ConsoleScenario scenario = ConsoleScenario.builder()
+                .read("enter 0 or 1")
+                .type("hui")
+                .read("wrong argument value: 0 or 1 is expected")
+                .read("enter 0 or 1")
+                .type("0")
+                .step("invoke command")
+                .read("command invoked")
+                .build();
+
+        final String key1 = UUID.randomUUID().toString();
+
+        new CommandHandlerImpl().handleCommand(
+                scenario::readLine,
+                scenario::writeLine,
+                new Command() {
+
+                    @Override
+                    public String getInvite() {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public List<Argument<?>> getArguments() {
+                        return List.of(
+                                new Argument<Integer>() {
+
+                                    @Override
+                                    public String getInvite() {
+                                        return "0 or 1";
+                                    }
+
+                                    @Override
+                                    public void convert(
+                                            final ConsoleReader reader,
+                                            final ArgumentCollector argumentCollector
+                                    ) throws ArgumentCaptureException {
+                                        final String line = reader.readLine();
+                                        try {
+                                            int i = Integer.parseInt(line);
+                                            if (i == 0 || i == 1) {
+                                                argumentCollector.add(key1, i);
+                                                return;
+                                            }
+                                        } catch (final Exception e) {
+                                            // ignore
+                                        }
+                                        throw new ArgumentCaptureException("0 or 1 is expected");
+                                    }
+
+                                    @Override
+                                    public Integer resolve(final ArgumentAccessor arguments) {
+                                        throw new UnsupportedOperationException();
+                                    }
+                                }
+                        );
+                    }
+
+                    @Override
+                    public void run(final ConsoleWriter writer, final ArgumentAccessor argumentAccessor) {
+                        scenario.checkStep("invoke command");
+
+                        Assert.assertEquals(0, argumentAccessor.get(key1));
+
+                        writer.writeLine("command invoked");
+                    }
+                }
+        );
+
+        scenario.checkFinish();
+    }
 }
